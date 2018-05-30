9f7c15f9eb35bd06fd949d7feb24381fbc93a635
Festus kiambi <kiamfestoh@gmail.com>
2018-05-30 10:20:25 +0300

    add files to cache and delete old caches in activate event listner

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,23 @@
 var cacheName = "weatherPWA-step-6-1";
-var filesToCache = [];
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/styles/inline.css',
+    '/images/clear.png',
+    '/images/cloudy-scattered-showers.png',
+    '/images/cloudy.png',
+    '/images/fog.png',
+    '/images/ic_add_white_24px.svg',
+    '/images/ic_refresh_white_24px.svg',
+    '/images/partly-cloudy.png',
+    '/images/rain.png',
+    '/images/scattered-showers.png',
+    '/images/sleet.png',
+    '/images/snow.png',
+    '/images/thunderstorm.png',
+    '/images/wind.png'
+];
 
 self.addEventListener('install',function(event){
     console.log('[ServiceWorker] Install');
@@ -12,5 +30,16 @@ self.addEventListener('install',function(event){
 });
 
 self.addEventListener('activate',function(event){
-    
+    event.waitUntil(
+        caches.keys().then(function(keyList){
+            return Promise.all(keyList.map(function(key){
+                if(!key==cacheName){
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+
+        })
+    );
+    return self.clients.claim();
 });
